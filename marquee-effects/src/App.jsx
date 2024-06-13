76228a97e4b4480a3ebd05fbc1a6b00de6f3b6e1
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-06-13 05:54:15 +0530

    added marquee effects

---
+++ marquee-effects/src/App.jsx
@@ -0,0 +1,31 @@
+import MarqueePage from "./components/Marquee";
+
+function App() {
+  // const row1 = [
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/7ae42bac3b34999c0db3.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/b2bd91d7b87b2181ca45.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/6591cdc0702b32310306.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3b7d9f4b073deb6a9b74.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3cd767dea94a85078ca4.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/a2b3c3709ffedce2a22a.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3cd767dea94a85078ca4.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/a2b3c3709ffedce2a22a.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3cd767dea94a85078ca4.png",
+  //   "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/a2b3c3709ffedce2a22a.png",
+  // ];
+
+  return (
+    <>
+      <div className="flex flex-col items-center h-screen justify-center">
+        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+
+        
+          <MarqueePage />
+
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ marquee-effects/src/components/Marquee.jsx
@@ -0,0 +1,57 @@
+import Marquee from "react-fast-marquee";
+
+
+function MarqueePage() {
+    return (
+
+  
+            <Marquee speed={200} loop={0} autoFill gradient={true} gradientWidth={200} gradientColor="white" className=" container overflow-hidden" >
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/7ae42bac3b34999c0db3.png"
+                        alt=""
+                    />
+                </div>
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/b2bd91d7b87b2181ca45.png"
+                        alt=""
+                    />
+                </div>
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/6591cdc0702b32310306.png"
+                        alt=""
+                    />
+                </div>
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3b7d9f4b073deb6a9b74.png"
+                        alt=""
+                    />
+                </div>
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3cd767dea94a85078ca4.png"
+                        alt=""
+                    />
+                </div>
+                <div className="m-4 w-40 h-24 flex justify-center rounded-lg items-center border border-transparent shadow-md p-4">
+                    <img
+                        className="object-contain w-full h-full"
+                        src="https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/a2b3c3709ffedce2a22a.png"
+                        alt=""
+                    />
+                </div>
+            </Marquee>
+   
+
+    )
+}
+
+export default MarqueePage
